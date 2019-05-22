acea0749ed553e3af2ad55fd696e81576f52051e
VanSickle-Wendi <gwendells@gmail.com>
2019-05-21 23:33:47 -0600

    Working

--- web/Week5/team.php
+++ web/Week5/team.php
@@ -45,6 +45,7 @@
             }            
             ?>
             
+            
             <p><span>John 1:5 - "And the light shineth in darkness; and the darkness comprehended it not."</span></p>
          </main>
          <footer>
