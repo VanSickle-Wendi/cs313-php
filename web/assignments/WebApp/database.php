0098cd98886b7e6d9b25b31b9c53364ddd864d5b
VanSickle-Wendi <gwendells@gmail.com>
2019-05-23 22:53:15 -0600

    Working on project

--- web/assignments/WebApp/database.php
+++ web/assignments/WebApp/database.php
@@ -13,7 +13,7 @@
          $dbHost = $dbOpts["host"];
          $dbPort = $dbOpts["port"];
          $dbUser = $dbOpts["user"];
-         $dbPassword = $dbOpts["pasrs"];
+         $dbPassword = $dbOpts["pass"];
          $dbName = ltrim($dbOpts["path"], '/');
 
          $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
