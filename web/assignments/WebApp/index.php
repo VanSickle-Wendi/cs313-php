0b1d2bcc40948170b5cb61b05260d0220b0212d6
VanSickle-Wendi <gwendells@gmail.com>
2019-05-25 00:48:09 -0600

    Project 1

--- web/assignments/WebApp/index.php
+++ web/assignments/WebApp/index.php
@@ -25,7 +25,7 @@ $db = get_db();
                <ul class="nav nav-pills pull-right">
                   <!-- I had to change line 1099 a{color} from #337AB7 to #fff to get white in the buttons -->
                   <li class="btn btn-primary"><a href="index.php">View List</a></li>
-                  <li role="presentation" class="btn btn-primary"><a href="view/songDetail.php">Song Details</a></li>
+                  <li role="presentation" class="btn btn-primary"><a href="views/songDetail.php">Song Details</a></li>
 
                </ul>
             </nav>
