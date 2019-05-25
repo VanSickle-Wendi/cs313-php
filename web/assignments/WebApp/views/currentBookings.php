39f58b72cdf6046ab8f432edb2c9969c7d55bec8
VanSickle-Wendi <gwendells@gmail.com>
2019-05-25 15:28:00 -0600

    project 1

--- web/assignments/WebApp/views/currentBookings.php
+++ web/assignments/WebApp/views/currentBookings.php
@@ -26,7 +26,6 @@ $db = get_db();
                <ul class="nav nav-pills pull-right">
                   <!-- I had to change line 1099 a{color} from #337AB7 to #fff to get white in the buttons -->
                   <li class="btn btn-primary"><a href="../index.php">View List</a></li>
-                  <li role="presentation" class="btn btn-primary"><a href="songDetail.php">Song Details</a></li>
                   <li role="presentation" class="btn btn-primary"><a href="meetSingers.php">The Singers</a></li>
                   <li role="presentation" class="btn btn-primary"><a href="currentBookings.php">Performances</a></li>
 
