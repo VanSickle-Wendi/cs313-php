bb69d73606475c7ff16fab9ccf94055898d79edb
VanSickle-Wendi <gwendells@gmail.com>
2019-05-28 19:05:02 -0600

    testing form

--- web/Week5_6/teamAssignment/list.php
+++ web/Week5_6/teamAssignment/list.php
@@ -3,11 +3,11 @@ $book = htmlspecialchars($_POST['book']);
 $chapter = htmlspecialchars($_POST['chapter']);
 $verse = htmlspecialchars($_POST['verse']);
 $content = htmlspecialchars($_POST['content']);
+$topic = htmlspecialchars($_POST['topic']);
 
 
 echo "$book\n";
 echo "$chapter\n";
 echo "$verse\n";
 echo "$content";
-?>
-
+echo "$major";
