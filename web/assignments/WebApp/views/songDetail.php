eb3b40576c753e8a190c294f2e893ae0ab2ad227
VanSickle-Wendi <gwendells@gmail.com>
2019-05-31 22:09:45 -0600

    Week 7 project

--- web/assignments/WebApp/views/songDetail.php
+++ web/assignments/WebApp/views/songDetail.php
@@ -44,7 +44,9 @@ $db = get_db();
 
                   <input type="submit" value="Submit"></p>
             </form>
+            <?php
             echo $sort;
+            ?>
          </div>
          <div class="row">
             <table class="table table-striped table-condensed">
