e5e7eeefae8a98c3976e23ffc5a8277048619b11
VanSickle-Wendi <gwendells@gmail.com>
2019-05-31 22:29:19 -0600

    Week 7 project

--- web/assignments/WebApp/views/songDetail.php
+++ web/assignments/WebApp/views/songDetail.php
@@ -79,6 +79,7 @@ $db = get_db();
                   //Get Songs
                   if(isset($sort) && $sort==="title") {
                   $statement = $db->prepare("SELECT title, tempo, genre, background FROM song ORDER BY title");
+                  }
                   $statement->execute();
                   while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
                      $title = $row['title'];
@@ -87,7 +88,6 @@ $db = get_db();
                      $background = $row['background'];
 
                      echo "<tr><td>$title</td><td>$tempo</td><td>$genre</td><td>$background</td></tr>";
-                  }
                   }
                   ?>
                   
@@ -95,6 +95,7 @@ $db = get_db();
                   //Get Songs
                   if(isset($sort) && $sort==="tempo") {
                   $statement = $db->prepare("SELECT title, tempo, genre, background FROM song ORDER BY tempo");
+                  }
                   $statement->execute();
                   while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
                      $title = $row['title'];
@@ -104,7 +105,7 @@ $db = get_db();
 
                      echo "<tr><td>$title</td><td>$tempo</td><td>$genre</td><td>$background</td></tr>";
                   }
-                  }
+
                   ?>
                   
                   <?php
