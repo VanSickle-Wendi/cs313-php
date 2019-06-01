a1744a2e7f5a462111af18fe90f43faaa187f058
VanSickle-Wendi <gwendells@gmail.com>
2019-06-01 12:57:05 -0600

    Week6 project

--- web/assignments/WebApp/views/formphp.php
+++ web/assignments/WebApp/views/formphp.php
@@ -1,8 +1,16 @@
 <?php
+require "../db/database.php";
+$db = get_db();
+//include('database.php');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$song_title = htmlspecialchars($_POST['song_title']);
+$artist = htmlspecialchars($_POST['artist']);
+
+$stmt = $db->prepare('INSERT INTO songsuggest(song_title, artist) VALUES(:song_title, :artist);');
+$stmt->bindValue(':song_title', $song_title, PDO::PARAM_STR);
+$stmt->bindValue(':artist', $artist, PDO::PARAM_STR);
+$stmt->execute();
+
+header("Location: suggestSong.php");
+?>
 
