353d691899abd1fe426417f20d2af95fd58bbd69
VanSickle-Wendi <gwendells@gmail.com>
2019-06-01 22:15:31 -0600

    Week6 Project

--- web/assignments/WebApp/views/phpBooked.php
+++ web/assignments/WebApp/views/phpBooked.php
@@ -8,9 +8,10 @@ $date = htmlspecialchars($_POST['date']);
 $time = htmlspecialchars($_POST['time']);
 $venue = htmlspecialchars($_POST['venue']);
 
-$stmt = $db->prepare('INSERT INTO performances(date, time) VALUES(:date, :time);');
+$stmt = $db->prepare('INSERT INTO performances(date, time, venue_id) VALUES(:date, :time :venue);');
 $stmt->bindValue(':date', $date, PDO::PARAM_STR);
 $stmt->bindValue(':time', $time, PDO::PARAM_STR);
+$stmt->bindValue(':venue', $venue, PDO::PARAM_STR);
 $stmt->execute();
 
 header("Location: book.php");
