234025bf9ccf5362facf39c57adb2a790c672a31
VanSickle-Wendi <gwendells@gmail.com>
2019-06-01 20:32:57 -0600

    Week6 Project

--- web/assignments/WebApp/views/delete.php
+++ web/assignments/WebApp/views/delete.php
@@ -4,4 +4,11 @@ $db = get_db();
 // Start Session
 session_start();
 
+//Get ID from URL
+$id = filter_var($_GET['id'],FILTER_VALIDATE_INT);
+
+$stmt = $db->prepare('DELETE FROM songsuggest WHERE id = $id;');
+$stmt->execute();
+
+header("Location: suggestSong.php");
 ?>
