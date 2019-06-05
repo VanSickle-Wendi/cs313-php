6e4908964092154735391da86d555e974777faf3
VanSickle-Wendi <gwendells@gmail.com>
2019-06-05 00:08:45 -0600

    Week7 Team Assignment

--- web/Week7/signinphp.php
+++ web/Week7/signinphp.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'dbConnect.php';
+include 'regphp.php';
 
 
 // Filter and store the data
@@ -10,27 +11,13 @@ $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
 // Compare the password just submitted against
 // the hashed password for the matching client
-$hashCheck = password_verify($password, $clientData['clientPassword']);
+$hashCheck = password_verify($password, $hashed_password);
 // If the hashes don't match create an error
 // and return to the login view
 if (!$hashCheck) {
-   $message = '<p class="notice">Please check your password and try again.</p>';
-   include '../view/login.php';
+   $message = '<p>Please check your password and try again.</p>';
+   include 'signin.php';
    exit;
 }
 
-// A valid user exists, log them in
-$_SESSION['loggedin'] = TRUE;
-setcookie('firstname', '', strtotime('-1 year'), '/');
-// Remove the password from the array
-// the array_pop function removes the last
-// element from an array
-array_pop($clientData);
-// Store the array into the session
-$_SESSION['clientData'] = $clientData;
-$_SESSION['message'] = "<p class='formMessage'>Welcome " . $_SESSION['clientData']['clientFirstname'] . "." . "</p>";
-// Send them to the admin view
-include '../view/admin.php';
-exit;
-break;
-
+header('Location: signin.php');
