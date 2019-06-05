205d8d0bd0d3ed2c8d4112d4bbf543cfb0d08421
VanSickle-Wendi <gwendells@gmail.com>
2019-06-05 00:22:05 -0600

    Week7 Team Assignment

--- web/Week7/signinphp.php
+++ web/Week7/signinphp.php
@@ -15,8 +15,10 @@ $hashCheck = password_verify($password, $hashed_password);
 // If the hashes don't match create an error
 // and return to the login view
 if (!$hashCheck) {
-   echo '<p>Please check your password and try again.</p>';
+   $message = '<p>Please check your password and try again.</p>';
    include 'signin.php';
+   echo "What?";
    exit;
-}else { header('Location: welcome.php');
+}else { 
+   header('Location: welcome.php');
 }
