cbf5bfcefcb8672af44814fd59d677ad0e3ae3ef
VanSickle-Wendi <gwendells@gmail.com>
2019-06-05 00:53:09 -0600

    Week7 Team Assignment

--- web/Week7/signinphp.php
+++ web/Week7/signinphp.php
@@ -17,19 +17,7 @@ $stmt->bindValue(':password', $hashed_password, PDO::PARAM_STR);
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 $hashed_password = $row['password'];
+echo $hashed_password;
 
 
 
-// Compare the password just submitted against
-// the hashed password for the matching client
-$hashCheck = password_verify($password, $hashed_password);
-// If the hashes don't match create an error
-// and return to the login view
-if (!$hashCheck) {
-   $message = '<p>Please check your password and try again.</p>';
-   include 'signin.php';
-   echo "What?";
-   exit;
-} else {
-   header('Location: welcome.php');
-}
