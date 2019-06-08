6ec4852a2e125fae4f3544547313685baa4c4387
VanSickle-Wendi <gwendells@gmail.com>
2019-06-08 13:38:42 -0600

    Finishing Project 1

--- web/assignments/WebApp/views/currentBookings.php
+++ web/assignments/WebApp/views/currentBookings.php
@@ -23,13 +23,7 @@ $db = get_db();
       <div class="container">
          <div class="header clearfix">
             <nav>
-               <ul class="nav nav-pills pull-right">
-                  <li class="btn btn-primary btn-xs"><a href="../index.php">View List</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="songDetail.php">Song Details</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="meetSingers.php">The Singers</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Performances</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest a Song</a></li>                  
-               </ul>
+               <?php include $_SERVER['DOCUMENT_ROOT'] . '/assignments/WebApp/common/nav.php'; ?>
             </nav>
             <h3 class="text-muted">Performances</h3>
             <form method="post" action="currentBookings.php">
