ae457dad9eae6375745e361cbc95d3555aa8a21e
VanSickle-Wendi <gwendells@gmail.com>
2019-06-08 17:08:18 -0600

    Finishing Project 1

--- web/assignments/WebApp/common/nav.php
+++ web/assignments/WebApp/common/nav.php
@@ -3,7 +3,7 @@
                   <li class="btn btn-primary btn-xs"><a href="songDetail.php">Song <br> Details</a></li>
                   <li class="btn btn-primary btn-xs"><a href="meetSingers.php">Meet the <br> Singers</a></li>
                   <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Current <br> Bookings</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest a <br> Song</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest <br> a Song</a></li>
                   <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Performances</a></li>
                   <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest a Song</a></li>                   
                </ul><br><br><br>
