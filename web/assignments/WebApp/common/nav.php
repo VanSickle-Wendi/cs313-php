3c40f67e20387bc1914fe492025ff41f857179e3
VanSickle-Wendi <gwendells@gmail.com>
2019-06-08 17:04:14 -0600

    Finishing Project 1

--- web/assignments/WebApp/common/nav.php
+++ web/assignments/WebApp/common/nav.php
@@ -1,8 +1,8 @@
                <ul class="nav nav-pills pull-right">
-                  <li class="btn btn-primary btn-xs"><a href="../index.php">View List</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="songDetail.php">Song Details</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="meetSingers.php">The Singers</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Performances</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="../index.php">Song <br> List</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="songDetail.php">Song <br> Details</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="meetSingers.php">Meet the <br> Singers</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Current <br> Bookings</a></li>
                   <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest a <br> Song</a></li>
                   <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Performances</a></li>
                   <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest a Song</a></li>                   
--- web/assignments/WebApp/common/navIndex.php
+++ web/assignments/WebApp/common/navIndex.php
@@ -1,9 +1,9 @@
                <ul class="nav nav-pills pull-right">
-                  <li class="btn btn-primary btn-xs"><a href="index.php">View List</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/songDetail.php">Song Details</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/meetSingers.php">The Singers</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/currentBookings.php">Performances</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest a Song</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="index.php">Song <br> List</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/songDetail.php">Song <br> Details</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/meetSingers.php">Meet the <br> Singers</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/currentBookings.php">Current <br> Bookings</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest a <br> Song</a></li>
                   <li class="btn btn-primary btn-xs"><a href="views/currentBookings.php">Performances</a></li>
                   <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest a Song</a></li>                   
                </ul><br><br><br>
