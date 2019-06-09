ed1795c29495ea8a190eaf245a46c266ba7be65b
VanSickle-Wendi <gwendells@gmail.com>
2019-06-08 18:44:07 -0600

    Finishing Project 1

--- web/assignments/WebApp/index.php
+++ web/assignments/WebApp/index.php
@@ -30,7 +30,7 @@ $db = get_db();
          <div>
             <form method="post" action="index.php">
 
-               <label for="song_num">Song #&nbsp;&nbsp;</label>
+               <label for="song_num" size="3">Song #&nbsp;&nbsp;</label>
                <input type="text" name="song_num" required>&nbsp;&nbsp;
 
                <label for="orig_artist">Original Artist&nbsp;&nbsp;</label>
