ee687b6b1398e55eac09af783d3ac3a3e392eb92
VanSickle-Wendi <gwendells@gmail.com>
2019-06-08 19:33:46 -0600

    Finishing Project 1

--- web/assignments/WebApp/views/updateForm.php
+++ web/assignments/WebApp/views/updateForm.php
@@ -8,6 +8,10 @@ session_start();
 $song_num = htmlspecialchars($_POST['song_num']);
 $orig_artist = htmlspecialchars($_POST['orig_artist']);
 $release_date = htmlspecialchars($_POST['release_date']);
+echo $song_num;
+echo $orig_artist;
+echo $release_date;
+
 
 $sql = 'UPDATE song SET orig_artist=:orig_artist, release_date=:release_date WHERE id=:id';
 $stmt = $db->prepare($sql);
@@ -15,6 +19,8 @@ $stmt->bindValue(':id', $song_num, PDO::PARAM_INT);
 $stmt->bindValue(':orig_artist', $orig_artist_title, PDO::PARAM_STR);
 $stmt->bindValue(':release_date', $release_date, PDO::PARAM_DATE);
 $stmt->execute();
+
+
 
 header("Location: ../index.php");
 die();
