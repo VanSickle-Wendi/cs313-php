ed0a4c7eee4c1d983445e3892952ea0f81a03c10
VanSickle-Wendi <gwendells@gmail.com>
2019-06-08 21:55:16 -0600

    Finishing Project 1

--- web/assignments/WebApp/common/navIndex.php
+++ web/assignments/WebApp/common/navIndex.php
@@ -1,8 +1,8 @@
                <ul class="nav nav-pills pull-right">
-                  <li class="btn btn-primary btn-xs"><a href="index.php">Song <br> List</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/songDetail.php">Song <br> Details</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/meetSingers.php">Meet the <br> Singers</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/currentBookings.php">Current <br> Bookings</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest <br> a Song</a></li>           
+                  <li class="btn btn-primary btn-xs"><a href="index.php">Song List</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/songDetail.php">Song Details</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/meetSingers.php">Meet the Singers</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/currentBookings.php">Current Bookings</a></li>
+                  <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest a Song</a></li>           
                </ul><br><br><br>
 
