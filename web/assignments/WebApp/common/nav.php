2b133d8ba2c68c0d23df8b25da2ff7fc99d92f8f
VanSickle-Wendi <gwendells@gmail.com>
2019-06-08 21:53:43 -0600

    Finishing Project 1

--- web/assignments/WebApp/common/nav.php
+++ web/assignments/WebApp/common/nav.php
@@ -3,8 +3,6 @@
                   <li class="btn btn-primary btn-xs"><a href="songDetail.php">Song <br> Details</a></li>
                   <li class="btn btn-primary btn-xs"><a href="meetSingers.php">Meet the <br> Singers</a></li>
                   <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Current <br> Bookings</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest <br> a Song</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="currentBookings.php">Performances</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest a Song</a></li>                   
+                  <li class="btn btn-primary btn-xs"><a href="suggestSong.php">Suggest <br> a Song</a></li>                
                </ul><br><br><br>
 
--- web/assignments/WebApp/common/navIndex.php
+++ web/assignments/WebApp/common/navIndex.php
@@ -3,8 +3,6 @@
                   <li class="btn btn-primary btn-xs"><a href="views/songDetail.php">Song <br> Details</a></li>
                   <li class="btn btn-primary btn-xs"><a href="views/meetSingers.php">Meet the <br> Singers</a></li>
                   <li class="btn btn-primary btn-xs"><a href="views/currentBookings.php">Current <br> Bookings</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest <br> a Song</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/currentBookings.php">Performances</a></li>
-                  <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest a Song</a></li>                   
+                  <li class="btn btn-primary btn-xs"><a href="views/suggestSong.php">Suggest <br> a Song</a></li>           
                </ul><br><br><br>
 
